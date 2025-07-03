8e531ed4228a3fd0a1413717e151b9871e3d386e
Lê Minh Trí <trilm189@gmail.com>
2025-07-03 17:23:30 +0700

    config syste dotnet microservice

---
+++ ApiGateway/API/Configuration/GatewayConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using System.Text;
+
+namespace API.Configuration
+{
+    public static class GatewayConfig
+    {
+        public static IServiceCollection AddGateWayConfig(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddOcelot(config);
+            services.AddSwaggerForOcelot(config);
+
+            var jwtSettings = config.GetSection("Jwt");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtSettings["Issuer"],
+
+                        ValidateAudience = true,
+                        ValidAudience = jwtSettings["Audience"],
+
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            return services;
+        }
+    }
+}
