8a89896fabb5eed068f381b4603315e81fbc7716
Giang Lê <collincomms@gmail.com>
2025-12-07 22:36:14 +0700

    [Fix] Temporary fix for unfinished function

--- src/PaymentService/Application/Features/Transactions/Commands/CreateGenericTransaction/CreateGenericTransactionCommandHandler.cs
+++ src/PaymentService/Application/Features/Transactions/Commands/CreateGenericTransaction/CreateGenericTransactionCommandHandler.cs
@@ -16,33 +16,35 @@ namespace PaymentService.Application.Features.Transactions.Commands.CreateGeneri
 
         public async Task<Result<Guid>> Handle(CreateGenericTransactionCommand request, CancellationToken cancellationToken)
         {
-            // Getting the source wallet
-            var sourceWallet = await unitOfWork.WalletRepository.GetByIdAsync(request.FromWalletId);
+            //// Getting the source wallet
+            //var sourceWallet = await unitOfWork.WalletRepository.GetByIdAsync(request.FromWalletId);
 
-            if (sourceWallet == null)
-            {
-                return Result<Guid>.Failure(ServiceError.NotFoundError($"{request.FromWalletId}"), "Wallet not found");
-            }
+            //if (sourceWallet == null)
+            //{
+            //    return Result<Guid>.Failure(ServiceError.NotFoundError($"{request.FromWalletId}"), "Wallet not found");
+            //}
 
-            // Getting the destination wallet
-            Domain.Entities.Wallet? destinationWallet = null;
+            //// Getting the destination wallet
+            //Domain.Entities.Wallet? destinationWallet = null;
 
-            if (request.TransactionInfo.ToWallet != null)
-            {
-                destinationWallet = await unitOfWork.WalletRepository.GetByIdAsync(request.TransactionInfo.ToWallet);
-            }
+            //if (request.TransactionInfo.ToWallet != null)
+            //{
+            //    destinationWallet = await unitOfWork.WalletRepository.GetByIdAsync(request.TransactionInfo.ToWallet);
+            //}
             
-            if (request.TransactionInfo.UpdateBalance && destinationWallet == null)
-            {
-                return Result<>
-            }
+            //if (request.TransactionInfo.UpdateBalance && destinationWallet == null)
+            //{
+            //    return Result<>
+            //}
 
-            // Preparing the transaction
-            Transaction newTransaction = new Transaction
-            {
-                FromWalletId = request.FromWalletId,
-                ToWalletId = request.TransactionInfo.ToWallet,
-            }
+            //// Preparing the transaction
+            //Transaction newTransaction = new Transaction
+            //{
+            //    FromWalletId = request.FromWalletId,
+            //    ToWalletId = request.TransactionInfo.ToWallet,
+            //}
+
+            throw new NotImplementedException();
         }
     }
 }
